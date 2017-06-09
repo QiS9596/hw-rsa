5919cb0289f807a69645fc1bf41cfeb6652f115b
Rolo <rolo_lamperouge@foxmail.com>
2017-06-09 19:24:11 +0800

    complete keycode generation

--- rsa.py
+++ rsa.py
@@ -1,4 +1,5 @@
 import random
+import fractions
 class keyGenerator:
     lst = []
     def __init__(self):
@@ -53,3 +54,43 @@ class keyGenerator:
                 if self.isProbablePrime(p):
                     return p
                 else: p = p+2
+
+    def keyGeneration(self,digit):
+        p = self.primeGeneration(digit)
+        q = self.primeGeneration(digit)
+        n = p*q
+        phin = (p-1)*(q-1)
+        e = phin-1
+        for i in range(2,phin -1):
+            if(fractions._gcd(i,phin) == 1):
+                e = i
+                break
+        privateKey = 0
+        for i in range(1,1000):
+            product = i*phin+1
+            print(product)
+            if(product%e == 0):
+                privateKey = product
+                break
+        file = open('n.txt','w')
+        file.write(n.__str__())
+        file.close()
+        file = open('e.txt','w')
+        file.write(e.__str__())
+        file.close()
+        file = open('d.txt','w')
+        file.write(privateKey.__str__())
+        file.close()
+
+def squareAndMultiply(exponent, base, modulus):
+    exponent2 = bin(exponent)
+    exponent2 = exponent2[2:exponent2.__len__()]
+    result = 1
+    for i in range(exponent2.__len__()-1, -1, -1):
+        result = result*result%modulus
+        if exponent2[i] == '1':
+            result = result*base%modulus
+    return result
+
+a = keyGenerator()
+a.keyGeneration(500)
